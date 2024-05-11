05b653440a81f2951962e31b3f3b2c28a11ce6d5
parkjise <monories79@gmail.com>
2024-05-11 15:08:35 +0900

    expand

---
+++ Split Landing Page/assets/js/script.js
@@ -0,0 +1,20 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const leftBtn = document.querySelector('.left > .btn');
+const rightBtn = document.querySelector('.right > .btn');
+const container = document.querySelector('.container');
+
+
+left.addEventListener('mouseenter',() => {
+  container.classList.add('hover-left');
+})
+left.addEventListener('mouseleave',() => {
+  container.classList.remove('hover-left');
+})
+
+right.addEventListener('mouseenter',() => {
+  container.classList.add('hover-right');
+})
+right.addEventListener('mouseleave',() => {
+  container.classList.remove('hover-right');
+})
